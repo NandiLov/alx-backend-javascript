6cc93de1a39fd6fc0f1b4b04458484e5fbb87b3f
NandiLov <nandilamul@gmail.com>
2023-10-12 10:52:25 +0200

    remove unnecessary files

--- 0x00-python_variable_annotations/3-to_str.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-'''a type-annotated function to_str that takes a float n as argument and returns the string representation of the float.
-'''
-
-
-def to_str(n: float) -> str:
-    '''Converts a floating-point number to a string.
-    '''
-    return str(n)
